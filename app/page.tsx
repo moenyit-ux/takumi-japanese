c9163d245aebcb98a1976bab6480c739694892d8
moenyit-ux <moe.nyit@gmail.com>
2026-08-17 21:41:42 +0900

    Link homepage testimonials nav to dedicated page

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
               <Link href="/profil/visi-misi">Visi &amp; Misi</Link>
               <Link href="/profil/profil-pendiri">Profil Pendiri</Link>
               <Link href="/profil/pengajar">Pengajar</Link>
-              <Link href="/profil#testimoni">Testimoni</Link>
+              <Link href="/profil/testimoni">Testimoni</Link>
             </div>
           </details>
           <details className="courseNav">
