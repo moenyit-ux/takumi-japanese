7d036feac9b8b2e619c7407dc2f96980837c6dbd
moenyit-ux <moe.nyit@gmail.com>
2026-08-17 21:38:31 +0900

    Fix teachers link on founder profile

--- app/profil/profil-pendiri/page.tsx
+++ app/profil/profil-pendiri/page.tsx
@@ -29,7 +29,7 @@ export default function FounderProfilePage() {
         <nav>
           <Link href="/profil/visi-misi">Visi &amp; Misi</Link>
           <Link href="/profil/profil-pendiri">Profil Pendiri</Link>
-          <Link href="/profil#pengajar">Pengajar</Link>
+          <Link href="/profil/pengajar">Pengajar</Link>
           <Link href="/profil#testimoni">Testimoni</Link>
         </nav>
         <Link className="btn ghost" href="/">Beranda</Link>
